f1ece2d76e842df76e55250ba182feec56a271fd
saloni modi <saloni.citrusbug@gmail.com>
2022-09-09 02:22:55 -0700

    test

---
+++ site.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('.card').mouseover(function () {
+        $('#pp1').css({
+                
+                "color": "#ffff",
+                "background": "blue",
+                // "border":"3px solid #ccc"
+            });
+
+    });
+    $('.card').mouseover(function () {
+        $('#pp1').css({
+                
+                "color": "#ffff",
+                "background": "blue",
+                // "border":"3px solid #ccc"
+            });
+
+    });
+});
